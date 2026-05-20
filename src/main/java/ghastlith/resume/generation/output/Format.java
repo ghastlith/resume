15bbd082c33be5cfa45c45c1578093c23b5454c0
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-20 17:41:21 -0300

    feat: add boolean logic validation to match enum values

--- src/main/java/ghastlith/resume/generation/output/Format.java
+++ src/main/java/ghastlith/resume/generation/output/Format.java
@@ -7,6 +7,17 @@ public enum Format {
 
   ALL,
   PDF,
-  MD,
+  MD;
+
+  /**
+   * Check if current enum value is ALL or if it is target format for boolean
+   * logic operations as ALL represents every possible format.
+   *
+   * @param target the target format to be checked against current value
+   * @return If current value includes target format.
+   */
+  public boolean includes(final Format target) {
+    return this == ALL || this == target;
+  }
 
 }
