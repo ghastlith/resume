7839ef57136253774877575f022636f9c0ee5195
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-20 06:07:21 -0300

    refactor: make language enum lowercase

--- src/main/java/ghastlith/resume/generation/output/Language.java
+++ src/main/java/ghastlith/resume/generation/output/Language.java
@@ -1,25 +1,11 @@
 package ghastlith.resume.generation.output;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 /**
  * The language in which resume sections and specific keywords will be rendered.
  */
 public enum Language {
 
-  EN_US,
-  PT_BR;
-
-  private static final String DASH = "-";
-  private static final String UNDERSCORE = "_";
-
-  @JsonCreator
-  public static Language from(final String value) {
-    final var language = value.trim()
-        .toUpperCase()
-        .replace(DASH, UNDERSCORE);
-
-    return Language.valueOf(language);
-  }
+  en,
+  pt,
 
 }
