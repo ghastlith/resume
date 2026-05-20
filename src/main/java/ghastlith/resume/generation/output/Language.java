5c958e099890a1a71aa21058563a12893d73aa7b
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-20 06:10:42 -0300

    refactor: change lower case implementation of language to match modern standards

--- src/main/java/ghastlith/resume/generation/output/Language.java
+++ src/main/java/ghastlith/resume/generation/output/Language.java
@@ -1,11 +1,18 @@
 package ghastlith.resume.generation.output;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * The language in which resume sections and specific keywords will be rendered.
  */
+@Getter
+@RequiredArgsConstructor
 public enum Language {
 
-  en,
-  pt,
+  EN("en"),
+  PT("pt");
+
+  private final String code;
 
 }
